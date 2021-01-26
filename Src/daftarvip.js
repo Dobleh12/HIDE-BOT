7cc4d004f59f5e8f17cae0967c0ffcb4e2dc58d2
Dobleh12 <76861967+Dobleh12@users.noreply.github.com>
2021-01-26 15:18:20 +0700

    Add files via upload

---
+++ Src/daftarvip.js
@@ -0,0 +1,17 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 5K > Akses Fitur ViP
+-Rp. 10K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_wa.me/6285810462881
+
+*NOTE*
+
+*GRUP WHATSAPP BOT :*
+
+}
+exports.daftarvip = daftarvip
